cf92ca581c433ac8a8b9de4b83d6cb4ef99904ea
MCMrARM <mcmrarm@gmail.com>
2018-05-25 21:43:39 +0200

    Add --help

--- include/argparser/arg_parser.h
+++ include/argparser/arg_parser.h
@@ -13,9 +13,17 @@ public:
     using handler = std::function<void (arg_list&)>;
 
 private:
+    struct help_entry {
+        std::string name, shortname, desc;
+    };
     std::unordered_map<std::string, handler> handlers;
+    std::vector<help_entry> help_entries;
 
 public:
+    arg_parser() {
+        help_entries.push_back({"--help", "-h", "Show this help information"});
+    }
+
     bool parse(int argc, const char** argv) {
         arg_list list (argc, argv);
         if (list.next_or_null() == nullptr)
@@ -24,6 +32,10 @@ public:
         while ((key = list.next_or_null()) != nullptr) {
             auto handler = handlers.find(key);
             if (handler == handlers.end()) {
+                if (strcmp(key, "-h") == 0 || strcmp(key, "--help") == 0) {
+                    print_help();
+                    return false;
+                }
                 printf("Unknown argument: %s\n", key);
                 return false;
             }
@@ -39,6 +51,13 @@ public:
 
     void add_arg(std::string const& name, std::string const& shortname, std::string const& desc, handler h) {
         handlers[name] = handlers[shortname] = h;
+        help_entries.push_back({name, shortname, desc});
+    }
+
+    void print_help() {
+        printf("Program Help\n");
+        for (auto const& e : help_entries)
+            printf("%-2s %-15s %s\n", e.shortname.c_str(), e.name.c_str(), e.desc.c_str());
     }
 
 };
