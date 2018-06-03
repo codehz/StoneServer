22defcc6345e0bf262c615ff68ab68de49812eda
MCMrARM <mcmrarm@gmail.com>
2018-06-03 21:33:24 +0200

    Import updates from the legacy repo (1.4 changes required for the server to work)

--- src/minecraft/ServerInstance.h
+++ src/minecraft/ServerInstance.h
@@ -19,36 +19,28 @@ class ResourcePackManager;
 class ContentTierManager;
 class FilePathManager;
 class IContentKeyProvider;
-
-class NetworkHandler {
-
-public:
-
-    char filler[0x200];
-
-    NetworkHandler();
-
-};
+class LevelStorage;
+class Scheduler;
+class LevelData;
 
 class ServerInstance {
 
 public:
 
-    char filler[0x8];
+    char filler[0x10];
     Minecraft* minecraft;
     char filler2[0x200];
 
-    /// @symbol _ZN14ServerInstanceC2ER13IMinecraftAppRK9WhitelistRK7OpsListP15FilePathManagerNSt6chrono8durationIxSt5ratioILx1ELx1EEEESsSsSsRK19IContentKeyProviderSs13LevelSettingsRN9minecraft3api3ApiEibiiibRKSt6vectorISsSaISsEESsRKN3mce4UUIDER17MinecraftEventingR14NetworkHandlerR22ResourcePackRepositoryRK18ContentTierManagerR19ResourcePackManagerPS15_St8functionIFvRKSsEE
-    ServerInstance(IMinecraftApp&, Whitelist const&, OpsList const&, FilePathManager*, std::chrono::seconds, mcpe::string, mcpe::string, mcpe::string, IContentKeyProvider const&, mcpe::string, LevelSettings, minecraft::api::Api&, int, bool, int, int, int, bool, std::vector<mcpe::string> const&, mcpe::string, mce::UUID const&, MinecraftEventing&, NetworkHandler&, ResourcePackRepository&, ContentTierManager const&, ResourcePackManager&, ResourcePackManager*, std::function<void (mcpe::string const&)>);
+    /// @symbol _ZN14ServerInstanceC2ER13IMinecraftAppR9WhitelistRK7OpsListP15FilePathManagerNSt6chrono8durationIxSt5ratioILx1ELx1EEEESsSsSs13LevelSettingsRN9minecraft3api3ApiEibiiibRKSt6vectorISsSaISsEESsRKN3mce4UUIDER17MinecraftEventingR22ResourcePackRepositoryRK18ContentTierManagerR19ResourcePackManagerSt8functionIFSt10unique_ptrI12LevelStorageSt14default_deleteIS13_EER9SchedulerEERKSsP9LevelDataPSZ_S11_IFvS1C_EES1H_
+    ServerInstance(IMinecraftApp&, Whitelist&, OpsList const&, FilePathManager*, std::chrono::seconds, mcpe::string, mcpe::string, mcpe::string, LevelSettings, minecraft::api::Api&, int, bool, int, int, int, bool, std::vector<mcpe::string> const&, mcpe::string, mce::UUID const&, MinecraftEventing&, ResourcePackRepository&, ContentTierManager const&, ResourcePackManager&, std::function<std::unique_ptr<LevelStorage> (Scheduler&)>, mcpe::string const&, LevelData*, ResourcePackManager*, std::function<void (mcpe::string const&)>, std::function<void (mcpe::string const&)>);
 
     ~ServerInstance();
 
-    void update();
+    void startServerThread();
 
-    void startLeaveGame();
+    void leaveGameSync();
 
-    bool isLeaveGameDone() const;
-
-    void mainThreadNetworkUpdate_HACK();
+    /// @symbol _ZN14ServerInstance20queueForServerThreadESt8functionIFvvEE
+    void queueForServerThread(std::function<void ()>);
 
 };
