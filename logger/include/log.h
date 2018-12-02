04b8cfc650642e30db52cc917606a95d683f0d38
CodeHz <codehz@outlook.com>
2018-12-02 21:43:00 +0800

    feat(logger): add i18n for level

--- logger/include/log.h
+++ logger/include/log.h
@@ -1,54 +1,43 @@
 #pragma once
 
+#include <cstdarg>
 #include <cstdlib>
 #include <uintl.h>
 #include <vector>
-#include <cstdarg>
 
-#define LogFuncDef(name, logLevel) \
-    static void name(const char* tag, const char* text, ...) __attribute__ ((format (printf, 2, 3))) { \
-        va_list args; \
-        va_start(args, text); \
-        vlog(logLevel, tag, text, args); \
-        va_end(args); \
-    }
+#define LogFuncDef(name, logLevel)                                                                                                                   \
+  static void name(const char *tag, const char *text, ...) __attribute__((format(printf, 2, 3))) {                                                   \
+    va_list args;                                                                                                                                    \
+    va_start(args, text);                                                                                                                            \
+    vlog(logLevel, tag, text, args);                                                                                                                 \
+    va_end(args);                                                                                                                                    \
+  }
 
-enum class LogLevel {
-    LOG_TRACE,
-    LOG_DEBUG,
-    LOG_INFO,
-    LOG_WARN,
-    LOG_ERROR
-};
+enum class LogLevel { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_MAX };
+
+
 
 class Log {
 
 public:
+  static inline const char *getLogLevelString(LogLevel lvl) {
+    using namespace uintl;
+    static char const *lvmap[] = {"Trace"_static_intl, "Debug"_static_intl, "Info "_static_intl, "Warn "_static_intl, "Error"_static_intl};
+    if (lvl >= LogLevel::LOG_MAX) return "?";
+    return lvmap[(int)lvl];
+  }
 
-    static inline const char *getLogLevelString(LogLevel lvl) {
-        if (lvl == LogLevel::LOG_TRACE) return "Trace";
-        if (lvl == LogLevel::LOG_DEBUG) return "Debug";
-        if (lvl == LogLevel::LOG_INFO) return "Info";
-        if (lvl == LogLevel::LOG_WARN) return "Warn";
-        if (lvl == LogLevel::LOG_ERROR) return "Error";
-        return "?";
-    }
+  static void vlog(LogLevel level, const char *tag, const char *text, va_list args);
 
-    static void vlog(LogLevel level, const char* tag, const char* text, va_list args);
+  static void log(LogLevel level, const char *tag, const char *text, ...) {
+    va_list args;
+    va_start(args, text);
+    vlog(level, tag, text, args);
+    va_end(args);
+  }
 
-    static void log(LogLevel level, const char* tag, const char* text, ...) {
-        va_list args;
-        va_start(args, text);
-        vlog(level, tag, text, args);
-        va_end(args);
-    }
-
-    LogFuncDef(trace, LogLevel::LOG_TRACE)
-    LogFuncDef(debug, LogLevel::LOG_DEBUG)
-    LogFuncDef(info, LogLevel::LOG_INFO)
-    LogFuncDef(warn, LogLevel::LOG_WARN)
-    LogFuncDef(error, LogLevel::LOG_ERROR)
-
+  LogFuncDef(trace, LogLevel::LOG_TRACE) LogFuncDef(debug, LogLevel::LOG_DEBUG) LogFuncDef(info, LogLevel::LOG_INFO)
+      LogFuncDef(warn, LogLevel::LOG_WARN) LogFuncDef(error, LogLevel::LOG_ERROR)
 };
 
 #undef LogFuncDef
--- logger/src/log.cpp
+++ logger/src/log.cpp
@@ -2,21 +2,19 @@
 #include <cstdio>
 #include <ctime>
 
-void Log::vlog(LogLevel level, const char* tag, const char* text, va_list args) {
-    char buffer[4096];
-    int len = vsnprintf(buffer, sizeof(buffer), uintl::gettext(text), args);
-    if (len > sizeof(buffer))
-        len = sizeof(buffer);
-    while (len > 0 && (buffer[len - 1] == '\r' || buffer[len - 1] == '\n'))
-        buffer[--len] = '\0';
+void Log::vlog(LogLevel level, const char *tag, const char *text, va_list args) {
+  char buffer[4096];
+  int len = vsnprintf(buffer, sizeof(buffer), uintl::gettext(text), args);
+  if (len > sizeof(buffer)) len = sizeof(buffer);
+  while (len > 0 && (buffer[len - 1] == '\r' || buffer[len - 1] == '\n')) buffer[--len] = '\0';
 
-    char tbuf[128];
-    tbuf[0] = '\0';
+  char tbuf[128];
+  tbuf[0] = '\0';
 
-    time_t t = time(nullptr);
-    tm tm;
-    localtime_r(&t, &tm);
-    strftime(tbuf, sizeof(tbuf), "%H:%M:%S", &tm);
-    printf("%s %-5s [%s] %s\n", tbuf, getLogLevelString(level), uintl::gettext(tag), buffer);
-    fflush(stdout);
+  time_t t = time(nullptr);
+  tm tm;
+  localtime_r(&t, &tm);
+  strftime(tbuf, sizeof(tbuf), "%H:%M:%S", &tm);
+  printf("%s %s [%s] %s\n", tbuf, getLogLevelString(level), uintl::gettext(tag), buffer);
+  fflush(stdout);
 }
