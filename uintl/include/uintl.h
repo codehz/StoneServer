e7f655946f16a0dc98fec1051c7a189a9d7a48e9
CodeHz <codehz@outlook.com>
2018-12-02 21:41:32 +0800

    feat(uintl): add static_intl

--- uintl/include/uintl.h
+++ uintl/include/uintl.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <cstddef>
 #include <iostream>
 #include <locale>
@@ -8,6 +9,16 @@
 
 namespace uintl {
 char const *operator""_intl(char const *inp, std::size_t);
+#ifdef VSCODE_WORKAROUND
+char const *operator""_static_intl(char const *inp, std::size_t);
+#else
+template <char... X> char const *operator""_static_intl_in() {
+  static char data[] = { X..., 0 };
+  static std::string converted = operator""_intl(data, -1);
+  return converted.c_str();
+}
+template <typename T, T... X> char const *operator""_static_intl() { return operator""_static_intl_in<X...>(); }
+#endif
 char const *gettext(char const *inp);
 void intl_domain(char const *domain);
 } // namespace uintl
