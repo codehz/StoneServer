49b7c9148402635aa345992fda9b98c77e9b72a1
Code Hz <CodeHz@outlook.com>
2018-12-07 19:13:56 +0800

    project: add Seasocks and fix
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- stoned/src/main.cpp
+++ stoned/src/main.cpp
@@ -5,6 +5,14 @@
 
 #include <csignal>
 
-int main() {
+#include <seasocks/PrintfLogger.h>
+#include <seasocks/Server.h>
 
+#include <memory>
+
+int main() {
+  using namespace seasocks;
+  auto logger = std::make_shared<PrintfLogger>();
+  Server server(logger);
+  server.serve("web", 9090);
 }
