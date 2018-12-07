1f28d3adb3133a6c6825c0c3e233ad822070c096
Code Hz <CodeHz@outlook.com>
2018-12-07 14:52:28 +0800

    git subrepo clone --branch=master https://github.com/minecraft-linux/minecraft-imported-symbols deps/minecraft-imported-symbols
    subrepo:
      subdir:   "deps/minecraft-imported-symbols"
      merged:   "efbf38e"
    upstream:
      origin:   "https://github.com/minecraft-linux/minecraft-imported-symbols"
      branch:   "master"
      commit:   "efbf38e"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ deps/minecraft-imported-symbols/include/minecraft/imported/egl_symbols.h
@@ -0,0 +1,20 @@
+#pragma once
+
+const char* egl_symbols[] = {
+        "eglChooseConfig",
+        "eglGetError",
+        "eglCreateWindowSurface",
+        "eglGetConfigAttrib",
+        "eglCreateContext",
+        "eglDestroySurface",
+        "eglSwapBuffers",
+        "eglMakeCurrent",
+        "eglDestroyContext",
+        "eglTerminate",
+        "eglGetDisplay",
+        "eglInitialize",
+        "eglQuerySurface",
+        "eglSwapInterval",
+        "eglQueryString",
+        nullptr
+};
