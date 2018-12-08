a3dbb50fa2041baf79eab88bf30ddcebfae568bf
Code Hz <CodeHz@outlook.com>
2018-12-08 13:21:14 +0800

    fix(stoned): revent
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- stoned/src/main.cpp
+++ stoned/src/main.cpp
@@ -6,33 +6,17 @@
 #include <seasocks/PrintfLogger.h>
 #include <seasocks/Server.h>
 
-#include <msgpack.hpp>
-
 #include <csignal>
 #include <memory>
 
 using namespace seasocks;
 using namespace std;
 
-struct WSCommand {
-  string name, content;
-  MSGPACK_DEFINE(name, content);
-};
-
 struct StoneHandler : WebSocket::Handler {
   set<WebSocket *> connections;
   void onConnect(WebSocket *socket) override { connections.insert(socket); }
   void onDisconnect(WebSocket *socket) override { connections.erase(socket); }
   void onData(WebSocket *socket, const char *data) override {
-    auto handle = msgpack::unpack(data, -1);
-    WSCommand command;
-    try {
-      handle.get().convert(command);
-      printf("%s: %s\n", command.name.c_str(), command.content.c_str());
-    } catch (bad_cast e) {
-      printf("error in cast: %s\n", e.what());
-      socket->close();
-    }
   }
 };
 
