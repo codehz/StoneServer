f37b8f1f2507913c047921f8fb5c02588441e797
Code Hz <CodeHz@outlook.com>
2018-12-14 17:01:47 +0800

    feat(interface): remove unused broadcast feature
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- interface/include/interface/chat.h
+++ interface/include/interface/chat.h
@@ -11,11 +11,6 @@ struct Chat : BaseInterface {
   bool intercept;
 
   EventEmitter<Player &, std::string const &> onPlayerChat;
-  std::function<void(std::string const &, std::string const &, std::string const &, std::string const &)> broadcastChat;
-  std::function<void(std::string const &, std::string const &, std::string const &, std::string const &)> broadcastAnnouncement;
-  std::function<void(std::string const &)> broadcastSystemMessage;
-  std::function<void(std::string const &)> broadcastJukebox;
-  std::function<void(std::string const &)> broadcastWhisper;
 };
 
 } // namespace interface
