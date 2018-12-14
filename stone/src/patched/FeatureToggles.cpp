0b4df5cf90f4f9e6657b429ebf731eaf56f113fd
Code Hz <CodeHz@outlook.com>
2018-12-14 19:55:15 +0800

    fix(stone): shutup Feature toggles
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- stone/src/patched/FeatureToggles.cpp
+++ stone/src/patched/FeatureToggles.cpp
@@ -10,8 +10,11 @@ struct Option {
 };
 
 SClasslessInstanceHook(bool, _ZNK14FeatureToggles9isEnabledE15FeatureOptionID, int id) {
-  Option *ptr = *(Option **)((int) this + id * 4);
+  static bool notified[256] = { 0 };
+  if (notified[id]) return true;
+  Option *ptr = *(Option **)((int)this + id * 4);
   Log::info("FeatureToggles", "Feature (%s): %d\n", ptr->name.c_str(), id);
+  notified[id] = true;
   return true;
 }
 
