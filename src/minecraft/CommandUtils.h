5c9f10abfbd881f9a96144185e157551f1341be5
Code Hz <CodeHz@outlook.com>
2018-12-24 17:44:42 +0800

    feat(scripting): registerCommand string parameter
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- src/minecraft/CommandUtils.h
+++ src/minecraft/CommandUtils.h
@@ -3,3 +3,13 @@
 #include "CommandRegistry.h"
 
 using CDT = void (*)(void *);
+
+template<typename> class CommonType;
+
+template<>
+struct CommonType<mcpe::string> {
+  /// @symbol _ZZ7type_idI15CommandRegistrySsE8typeid_tIT_EvE2id
+  static typeid_t<CommandRegistry> tid;
+  /// @symbol _ZNK15CommandRegistry5parseISsEEbPvRKNS_10ParseTokenERK13CommandOriginiRSsRSt6vectorISsSaISsEE
+  static Parser parser;
+};
