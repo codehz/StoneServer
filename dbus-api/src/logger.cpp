55d9a5fc9a4414c5d0b9ac0beb367b5e78018cfb
CodeHz <CodeHz@outlook.com>
2019-01-01 12:05:22 +0800

    feat(dbus-api): add logger test

---
+++ dbus-api/src/logger.cpp
@@ -0,0 +1,31 @@
+#include <dbus-api/CoreService.h>
+
+#include <simppl/dispatcher.h>
+#include <simppl/stub.h>
+
+#include <csignal>
+
+static simppl::dbus::Dispatcher disp("bus:session");
+
+int main() {
+  using namespace simppl::dbus;
+  using namespace one::codehz::stone;
+  
+  Stub<CoreService> core(disp, "default");
+  printf("waiting connection...\n");
+  core.connected >> [&] (ConnectionState state) {
+    if (state == ConnectionState::Connected) {
+      printf("connected!\n");
+      core.log.attach() >> [](int level, std::string const &tag, std::string const &content) {
+        printf("%d [%s] %s\n", level, tag.c_str(), content.c_str());
+      };
+    } else {
+      printf("disconnected!\n");
+      core.log.detach();
+      core.disp().stop();
+    }
+  };
+  std::signal(SIGINT, [](int) { disp.stop(); });
+  std::signal(SIGTERM, [](int) { disp.stop(); });
+  disp.run();
+}
