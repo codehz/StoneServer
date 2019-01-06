5ef09acc2739dad56e5b3b0f5e49e6702b72e974
Code Hz <CodeHz@outlook.com>
2019-01-06 20:06:23 +0800

    feat(dbus-api): add broadcast api
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- include/dbus-api/ChatService.h
+++ include/dbus-api/ChatService.h
@@ -8,11 +8,13 @@ using namespace simppl::dbus;
 
 INTERFACE(ChatService) {
   Method<in<string>, in<string>, oneway> send;
+  Method<in<string>, oneway> broadcast;
   Signal<string, string> recv;
   Property<bool> intercept;
 
   ChatService()
       : INIT(send)
+      , INIT(broadcast)
       , INIT(recv)
       , INIT(intercept) {}
 };
