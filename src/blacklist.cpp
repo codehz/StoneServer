4d637b65471752c46fe02277cb1f67435650d3fe
Code Hz <CodeHz@outlook.com>
2019-01-06 17:37:01 +0800

    feat(dbus-api): add by name
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- src/blacklist.cpp
+++ src/blacklist.cpp
@@ -1,4 +1,5 @@
 #include <dbus-api/BlacklistService.h>
+#include <dbus-api/CoreService.h>
 
 #include <simppl/dispatcher.h>
 #include <simppl/stub.h>
@@ -9,7 +10,7 @@
 
 static simppl::dbus::Dispatcher disp("bus:session");
 
-enum action { ADD_UUID, ADD_XUID, DEL_UUID, DEL_XUID };
+enum action { ADD_UUID, ADD_XUID, DEL_UUID, DEL_XUID, ADD_NAME };
 
 int main(int argc, char **argv) {
   using namespace simppl::dbus;
@@ -32,26 +33,64 @@ int main(int argc, char **argv) {
   } else if (strcmp(argv[1], "del-xuid") == 0) {
     assert(argc == 3);
     action = DEL_XUID;
+  } else if (strcmp(argv[1], "add-name") == 0) {
+    assert(argc == 4);
+    action = ADD_NAME;
   } else {
     fprintf(stderr, "add-uuid|add-xuid|del-uuid|del-xuid\n");
     return 2;
   }
 
   Stub<BlacklistService> blacklist(disp, "default");
+  Stub<CoreService> core(disp, "default");
   fprintf(stderr, "waiting connection...\n");
   blacklist.connected >> [&](ConnectionState state) {
     if (state == ConnectionState::Connected) {
       fprintf(stderr, "connected!\n");
       switch (action) {
-        case ADD_UUID: blacklist.addByUUID(argv[2], argv[3]); break;
-        case ADD_XUID: blacklist.addByXUID(argv[2], argv[3]); break;
-        case DEL_UUID: blacklist.removeByUUID(argv[2]); break;
-        case DEL_XUID: blacklist.removeByXUID(argv[2]); break;
+      case ADD_UUID:
+        blacklist.addByUUID(argv[2], argv[3]);
+        disp.stop();
+        break;
+      case ADD_XUID:
+        blacklist.addByXUID(argv[2], argv[3]);
+        disp.stop();
+        break;
+      case DEL_UUID:
+        blacklist.removeByUUID(argv[2]);
+        disp.stop();
+        break;
+      case DEL_XUID:
+        blacklist.removeByXUID(argv[2]);
+        disp.stop();
+        break;
+      case ADD_NAME: {
+        fprintf(stderr, "waiting connection(core)...\n");
+        core.connected >> [&](ConnectionState state) {
+          if (state == ConnectionState::Connected) {
+            fprintf(stderr, "connected(core)!\n");
+            core.players.get_async() >> [&](CallState call, vector<structs::PlayerInfo> const &vec) {
+              for (auto &player : vec) {
+                if (player.name == argv[2]) {
+                  blacklist.addByUUID(player.uuid, argv[3]);
+                  disp.stop();
+                  return;
+                }
+              }
+              fprintf(stderr, "playername not found: %s\n", argv[2]);
+              disp.stop();
+            };
+          } else {
+            fprintf(stderr, "disconnected(core)!\n");
+          }
+        };
+        break;
+      }
       }
     } else {
       fprintf(stderr, "disconnected!\n");
+      blacklist.disp().stop();
     }
-    blacklist.disp().stop();
   };
   std::signal(SIGINT, [](int) { disp.stop(); });
   std::signal(SIGTERM, [](int) { disp.stop(); });
