25fb0a4288e05ff098252723d180cc7953f4567b
Code Hz <CodeHz@outlook.com>
2019-02-08 11:32:05 +0800

    fix: packet
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- deps/minecraft-symbols/src/minecraft/Packet.h
+++ deps/minecraft-symbols/src/minecraft/Packet.h
@@ -1,9 +1,10 @@
 #pragma once
 
-struct  __attribute__ ((packed)) Packet {
+struct __attribute__((packed)) Packet {
   /// @symbol _ZTV6Packet
   static void *myVtable;
   void **vtable;       // 0
   int unk4, unk2;      // 4, 8
   unsigned char subId; // 12
+  void *handleChunk;   // 16
 };
