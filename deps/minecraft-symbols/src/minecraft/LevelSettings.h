a165f5b06c98f06bb46530a7099514bfe888e5f1
Code Hz <CodeHz@outlook.com>
2019-02-10 10:00:21 +0800

    fix: adjust offset
    Signed-off-by: Code Hz <CodeHz@outlook.com>

--- deps/minecraft-symbols/src/minecraft/LevelSettings.h
+++ deps/minecraft-symbols/src/minecraft/LevelSettings.h
@@ -5,33 +5,35 @@
 class LevelSettings {
 
 public:
+  int seed;                                                    // 4
+  int gametype;                                                // 8
+  int difficulty;                                              // c
+  bool forceGameType;                                          // 10
+  int generator;                                               // 14
+  bool achievementsDisabled;                                   // 18
+  int dimension;                                               // 1c
+  int time;                                                    // 20
+  bool edu, eduFeatures, immutableWorld;                       // 21, 22, 23
+  float rainLevel, lightningLevel;                             // 28, 2c
+  bool xblBroadcastIntent, mpGame, lanBroadcast, xblBroadcast; // 2d, 2e, 2f, 2e
+  int xblLanBroadcastMode;                                     // 34
+  int platformBroadcastIntent;                                 // 38
+  bool commandsEnabled;                                        // 39
+  bool texturepacksRequired;                                   // 3A
+  bool hasLockedBehaviorPack;                                  // 3B
+  bool hasLockedResourcePack;                                  // 3C
+  bool fromLockedTemplate;                                     // 3D
+  bool msaGamertagsOnly;                                       // 3E
+  bool overrideSavedSettings;                                  // 3F
+  bool bonusChestEnabled, startWithMap;                        // 40, 41
+  unsigned serverChunkTickRange;                               // 48
+  bool experimentalGameplay;                                   // 4C
+  char filler[0x94 - 0x4C];
+  int defaultSpawnX, defaultSpawnY, defaultSpawnZ;
+  char filler2[0x300];
 
-    int seed; // 4
-    int gametype; // 8
-    int difficulty; // c
-    bool forceGameType; // 10
-    int generator; // 14
-    bool achievementsDisabled; // 18
-    int dimension; // 1c
-    int time; // 20
-    bool edu, eduFeatures, immutableWorld; // 21, 22, 23
-    float rainLevel, lightningLevel; // 28, 2c
-    bool xblBroadcastIntent, mpGame, lanBroadcast, xblBroadcast; // 2d, 2e, 2f, 2e
-    int xblLanBroadcastMode; // 34
-    bool commandsEnabled; // 35
-    bool texturepacksRequired, lockedBehaviourPack, lockedResourcePack, fromLockedTemplate; // 36, 37, 38, 39
-    bool msaGamertagsOnly; // 3A
-    bool overrideSavedSettings; // 3B
-    bool bonusChestEnabled, startWithMap; // 3C, 3D
-    unsigned serverChunkTickRange; // 44
-    bool experimentalGameplay; // 45
-    char filler[0x94 - 0x45];
-    int defaultSpawnX, defaultSpawnY, defaultSpawnZ;
-    char filler2[0x300];
+  LevelSettings();
+  LevelSettings(LevelSettings const &org);
 
-    LevelSettings();
-    LevelSettings(LevelSettings const& org);
-
-    static int parseSeedString(mcpe::string const&, unsigned int);
-
+  static int parseSeedString(mcpe::string const &, unsigned int);
 };
