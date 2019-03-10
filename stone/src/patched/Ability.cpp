9de082f57a466baac73d07f78bc07c5633574ad9
Code Hz <CodeHz@outlook.com>
2019-03-10 18:01:30 +0800

    fix(ability)

--- stone/src/patched/Ability.cpp
+++ stone/src/patched/Ability.cpp
@@ -19,40 +19,46 @@ SHook(bool, _ZN6Player13canUseAbilityERKSs, Player *player, mcpe::string const &
 }
 
 SInstanceHook(bool, _ZN8GameMode12destroyBlockERK8BlockPosa, GameMode, BlockPos const &pos, signed char data) {
-  bool result = original(this, pos, data);
+  bool result = true;
   Locator<Policy>()->checkDestroy(this->player, pos, result);
-  return result;
+  if (result) return original(this, pos, data);
+  return false;
 }
 
 SInstanceHook(bool, _ZN8GameMode10buildBlockERK8BlockPosa, GameMode, BlockPos const &pos, signed char data) {
-  bool result = original(this, pos, data);
+  bool result = true;
   Locator<Policy>()->checkBuild(this->player, pos, result);
-  return result;
+  if (result) return original(this, pos, data);
+  return false;
 }
 
 SInstanceHook(bool, _ZN8GameMode7useItemER12ItemInstance, GameMode, ItemInstance &instance) {
-  bool result = original(this, instance);
+  bool result = true;
   Locator<Policy>()->checkUse(this->player, instance, result);
-  return result;
+  if (result) return original(this, instance);
+  return false;
 }
 
 SInstanceHook(bool, _ZN8GameMode9useItemOnER12ItemInstanceRK8BlockPosaRK4Vec3, GameMode, ItemInstance &instance, BlockPos const &pos,
               signed char data, Vec3 const &vec) {
-  bool result = original(this, instance, pos, data, vec);
+  bool result = true;
   Locator<Policy>()->checkUseOn(this->player, instance, pos, vec, result);
-  return result;
+  if (result) original(this, instance, pos, data, vec);
+  return false;
 }
 
 SInstanceHook(bool, _ZN8GameMode8interactER5ActorRK4Vec3, GameMode, Actor &target, Vec3 const &pos) {
-  bool result = original(this, target, pos);
+  bool result = true;
   Locator<Policy>()->checkInteract(this->player, target, pos, result);
-  return result;
+  if (result) original(this, target, pos);
+  return false;
 }
 
 SInstanceHook(bool, _ZN8GameMode6attackER5Actor, GameMode, Actor &target) {
-  bool result = original(this, target);
+  bool result = true;
   Locator<Policy>()->checkAttack(this->player, target, result);
-  return result;
+  if (result) original(this, target);
+  return false;
 }
 
 static patched::details::RegisterPatchInit pinit([] { Locator<Policy>().generate(); });
