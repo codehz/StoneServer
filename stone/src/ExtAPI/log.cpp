71c1a5a4e0bfab4c80a91ea4ea1899d0ee930c59
Code Hz <CodeHz@outlook.com>
2019-03-16 20:10:22 +0800

    feat(scripting): yet another log method

---
+++ stone/src/ExtAPI/log.cpp
@@ -0,0 +1,24 @@
+#include "common.h"
+
+namespace ExtAPI {
+
+static void logCallback(FunctionCallbackInfo<Value> const &info) {
+  auto iso = info.GetIsolate();
+  std::stringstream ss;
+  const auto max = info.Length();
+  for (auto i = 0;; i++) {
+    auto current      = info[i];
+    Local<String> str = current->IsString() ? Local<String>(current) : current->ToString(iso);
+    ss << V8Str(str);
+    if (i < max - 1) {
+      ss << ", ";
+    } else {
+      break;
+    }
+  }
+  Log::info("Scripting", "log: %s", ss.str().c_str());
+}
+
+static Register reg{ "registerComponent", "log", &logCallback };
+
+} // namespace ExtAPI
